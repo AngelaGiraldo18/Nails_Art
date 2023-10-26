9c7b1677ba13c8d5c70907914bfc786b3b4ea6d5
JUAn-EN <jnietooca@gmail.com>
2023-10-26 12:53:24 -0500

    agrego commit de front Nails art

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PrincipalComponent,
+    FooterComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
