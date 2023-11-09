4ebb12154a58eab08621ba61faa4668cc72c9ff8
JUAn-EN <jnietooca@gmail.com>
2023-11-09 05:30:08 -0500

    agrego commit de el formulario resgistro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from '../service/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  usuario = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    contrasena: '',
+    rol: 'cliente'
+  };
 
+  constructor(private usuarioService: UsuarioService) {}
+
+  registrarUsuario() {
+    console.log('Intentando registrar usuario:', this.usuario);
+    this.usuarioService.createUser(this.usuario).subscribe(
+      (response) => {
+        console.log('Usuario registrado con éxito:', response);
+        Swal.fire({
+          title: 'Registro exitoso',
+          text: 'El usuario se registró correctamente.',
+          icon: 'success',
+          confirmButtonText: 'OK'
+        });
+      },
+      (error) => {
+        console.error('Error al registrar usuario:', error);
+        Swal.fire({
+          title: 'Error',
+          text: 'Hubo un error al registrar el usuario. Por favor, inténtalo de nuevo.',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      }
+    );
+  }
 }
--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -1,26 +1,28 @@
+// Servicio (UsuarioService)
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable,of } from 'rxjs';
-@Injectable({
-  providedIn: 'root'
-})
-export class UserService {
-  private apiUrl = 'http://localhost:4000/api';
-  
+import { Observable, of } from 'rxjs';
 
-  constructor(private http: HttpClient) { }
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+  private apiUrl = 'http://localhost:4000/api';
+
+  constructor(private http: HttpClient) {}
 
   createUser(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/createusuario`, data);
+  }
+
+  authenticatedRequest(endpoint: string, data: any): Observable<any> {
     // Recupera el token de localStorage
     const token = localStorage.getItem('token');
-console.log('Token:', token);
-
 
     if (!token) {
       console.error('No se ha encontrado un token de autenticación.');
-      return of();
+      return of(); // Retorna un Observable vacío en lugar de undefined
     }
-    
 
     const httpOptions = {
       headers: new HttpHeaders({
@@ -28,7 +30,7 @@ console.log('Token:', token);
         'Authorization': 'Bearer ' + token
       })
     };
-    
-    return this.http.post(`${this.apiUrl}/createusuario`, data, httpOptions);
+
+    return this.http.post(`${this.apiUrl}/${endpoint}`, data, httpOptions);
   }
 }
